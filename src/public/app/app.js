d27e315687ac2767981d9630c746cb31143e4543
Dan Le <dhl1337@gmail.com>
2016-05-09 22:38:25 -0500

    add styles, map is up, and consoling input from inputbox

--- src/public/app/app.js
+++ src/public/app/app.js
@@ -2,17 +2,31 @@
  * Created by danle on 5/9/16.
  */
 require([
+    "dojo/dom",
+    "dojo/on",
+    "dojo/parser",
+    "dijit/registry",
     "esri/Map",
     "esri/views/MapView",
     "dojo/domReady!"
-], function(Map, MapView) {
+], function(dom, on, parser, registry, Map, MapView) {
     var map = new Map({
         basemap: "streets"
     });
+
     var view = new MapView({
         container: "viewDiv",  // Reference to the scene div created in step 5
         map: map,  // Reference to the map object created before the scene
-        zoom: 4,  // Sets the zoom level based on level of detail (LOD)
-        center: [15, 65]  // Sets the center point of view in lon/lat
+        zoom: 5,  // Sets the zoom level based on level of detail (LOD)
+        center: [-90.049, 37.485]  // Sets the center point of view in lon/lat
+    });
+
+    parser.parse();
+
+    var hashtag = registry.byId("hashtag");
+    var searchBtn = dom.byId("findBtn");
+
+    on(searchBtn, "click", function() {
+        console.log(hashtag.value);
     });
 });
