7bf32c9676db040b18ab1535aa8f2627a072570a
Dan Le <dhl1337@gmail.com>
2016-05-10 22:30:29 -0500

    weather map api and current location works

--- src/public/app/app.js
+++ src/public/app/app.js
@@ -4,12 +4,19 @@
 require([
     "dojo/dom",
     "dojo/on",
+    "dojo/request",
     "dojo/parser",
     "dijit/registry",
+    "esri/widgets/Locate",
     "esri/Map",
     "esri/views/MapView",
     "dojo/domReady!"
-], function(dom, on, parser, registry, Map, MapView) {
+], function(dom, on, request, parser, registry, Locate, Map, MapView) {
+    parser.parse();
+    var base_url = "https://api.instagram.com/v1",
+        zipCode = registry.byId("zipCode"),
+        searchBtn = dom.byId("findBtn");
+
     var map = new Map({
         basemap: "streets"
     });
@@ -17,16 +24,37 @@ require([
     var view = new MapView({
         container: "viewDiv",  // Reference to the scene div created in step 5
         map: map,  // Reference to the map object created before the scene
-        zoom: 5,  // Sets the zoom level based on level of detail (LOD)
+        zoom: 3,  // Sets the zoom level based on level of detail (LOD)
         center: [-90.049, 37.485]  // Sets the center point of view in lon/lat
     });
 
-    parser.parse();
-
-    var hashtag = registry.byId("hashtag");
-    var searchBtn = dom.byId("findBtn");
-
-    on(searchBtn, "click", function() {
-        console.log(hashtag.value);
+    var locateBtn = new Locate({
+        view: view
     });
+    locateBtn.startup();
+
+    view.ui.add(locateBtn, {
+        position: "top-left",
+        index: 0
+    });
+
+    var data;
+    on(searchBtn, "click", function() {
+        $.getJSON('http://api.openweathermap.org/data/2.5/weather?zip='+zipCode.value+',us&APPID=badf30671f9afee48fb9ede7d50f7e01', {
+            headers: {
+                "X-Requested-With": null
+            }
+        }).then(function(response){
+            data = response;
+            console.log(data);
+        });
+        var view = new MapView({
+            container: "viewDiv",  // Reference to the scene div created in step 5
+            map: map,  // Reference to the map object created before the scene
+            zoom: 6,  // Sets the zoom level based on level of detail (LOD)
+            center: [data.coord.lat, data.coord.lon]  // Sets the center point of view in lon/lat
+        });
+    });
+
+
 });
